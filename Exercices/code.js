7c24e487680596923c345d7a3b39ff417714e7f1
DavidSoulsand <futuro728@gmail.com>
2023-03-27 13:31:12 +0200

    Functions - Type

---
+++ Exercices/code.js
@@ -0,0 +1,26 @@
+/*function greet(name) {
+    let phrase = `Hey!   ${name} how are you?`;
+    document.write(phrase);
+}*/
+//const saludar = function(name) {
+//    let phrase = `Hey!   ${name} how are you?`;
+//   document.write(phrase);
+//}
+
+
+
+function plus(num1,num2) {
+    let suma = num1 + num2;
+    return suma;
+}
+document.write("<br>"+plus(12,0)+"<br>"+plus(12,22));
+
+const greet = (name)=>{
+    let phrase = `Hey! ${name} how are you?`
+    document.write(phrase)
+}
+greet("Peter")
+
+//const greet = name => document.write(phrase);
+//phrase("Lucas");
+
