e57b23a2ed0c6f9f1b803dc7dbfa6ddedce34397
DavidSoulsand <futuro728@gmail.com>
2023-04-20 16:32:20 +0200

    Problems Folder

---
+++ 1AAA-Problems/Problems-Cofla-Calculator/code.js
@@ -0,0 +1,24 @@
+
+const add = (num1,num2)=>{
+    return parseInt(num1) + parseInt(num2);
+}
+const subtraction = (num1,num2)=>{
+    return parseInt(num1) - parseInt(num2);
+}
+const multiplication = (num1,num2)=>{
+    return parseInt(num1) * parseInt(num2);
+}
+const division = (num1,num2)=>{
+    return parseInt(num1) / parseInt(num2);
+}
+
+alert("What do you need?");
+
+let operation = prompt("1: Add, 2: Substaction, 3: Multiplication, 4: Division");
+
+if (operation == 1) {
+    let number1 = prompt("First number to add?");
+    let number2 = prompt("First number to add?");
+    result = add(number1,number2);
+    alert("Your result is: "+result);
+}
