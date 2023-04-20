1bf824c49d2f924facc1cae8e6822089dbbf2693
DavidSoulsand <futuro728@gmail.com>
2023-04-20 15:31:23 +0200

    Transferencia de archivos

---
+++ Todos los Archivos/INTS/codeRound.js
@@ -0,0 +1,10 @@
+//let number = Math.fround(9.2312321);
+//let number = Math.trunc(9.919239193)
+//let number = Math.PI;
+//let number = Math.SQRT1_2
+//let number = Math.SQRT2;
+//let number = Math.E;
+//let number = Math.LN10;
+let number = Math.LOG10E;
+
+document.write(number);
---
+++ Todos los Archivos/INTS/codefor.js
@@ -0,0 +1,6 @@
+for (var i = 0; i < 100000; i++) {
+    let number = Math.random()*99;
+    number = Math.floor(number+1);
+    document.write(number + "<br>");
+    
+}
